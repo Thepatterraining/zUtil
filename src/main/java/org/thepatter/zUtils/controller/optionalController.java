a415b679eff93f3ba2be683e6338c274de8f5c4e
zhoutao <ztzhoutao041@163.com>
2023-03-06 15:34:28 +0800

    增加optional controller

---
+++ src/main/java/org/thepatter/zUtils/controller/optionalController.java
@@ -0,0 +1,37 @@
+package org.thepatter.zUtils.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@Slf4j
+@RequestMapping("/optional")
+public class optionalController {
+
+    @GetMapping("/else")
+    public int optionalElse() {
+        Optional<Integer> optionalInt = Optional.empty();
+        return optionalInt.orElseThrow(() -> {
+                return new RuntimeException("异常了");
+        });
+    }
+
+    @GetMapping("flatMap")
+    public String optionalMap() {
+        Optional<String> res = Optional.of(12).flatMap(this::doubleInt).flatMap(this::intToStr);
+        return res.get();
+    }
+
+    public Optional<String> intToStr(int x) {
+        return Optional.ofNullable(String.valueOf(x));
+    }
+
+    public Optional<Integer> doubleInt(int x) {
+        return Optional.ofNullable(x << 1);
+    }
+}
